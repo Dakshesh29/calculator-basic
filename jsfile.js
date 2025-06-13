864200c8dca5d3fc57e6514f4b118c66e9b78a36
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-06-13 16:43:47 +0530

    complete calculator UI with flex layout and styled buttons

--- jsfile.js
+++ jsfile.js
@@ -2,6 +2,36 @@ let firstNumber;
 let operator;
 let secondNumber;
 
-function operate(firstNumber,operator,secondNumber){
-
+function add(firstNumber,secondNumber){
+    return firstNumber + secondNumber
 }
+
+function substract(firstNumber, secondNumber){
+    return firstNumber - secondNumber
+}
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber * secondNumber
+}
+
+function divide(firstNumber, secondNumber){
+    if (b === 0) {
+        return "Cannot divide by zero!";
+    }
+    return a / b;
+}
+
+function operate(firstNumber, operator, secondNumber){
+    switch(operator){
+        case "+" :
+            return add(firstNumber,secondNumber)
+        case "-" :
+            return substract(firstNumber,secondNumber)
+        case "*":
+            return multiply(firstNumber,secondNumber)
+        case "/":
+            return divide(firstNumber,secondNumber)
+    }
+}
+
+console.log(operate(4,"+",6))
