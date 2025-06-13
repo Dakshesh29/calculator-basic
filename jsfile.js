d7605ae14091783cf74d7b036289be0c6e4dcc2b
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-06-13 15:46:48 +0530

    initial start

---
+++ jsfile.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+console.log(add(5,7))
+console.log(subtract(45,97))
+console.log(multiply(56,9))
+console.log(divide(45,9))
